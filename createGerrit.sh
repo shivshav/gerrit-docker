e9ddf3f1f5c963802a1367830006d67aa2d309b5
shiv <sprasad0603@gmail.com>
2015-10-19 15:30:28 -0700

    Made it so that the Gerrit stuff can be properly applied from the command line scripts being called and the ldap server gets properly set to the linked ldap server now

--- createGerrit.sh
+++ createGerrit.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
-GERRIT_WEBURL=${GERRIT_WEBURL:-$1}
-LDAP_SERVER=${LDAP_SERVER:-$2}
-LDAP_ACCOUNTBASE=${LDAP_ACCOUNTBASE:-$3}
-HTTPD_LISTENURL=${HTTPD_LISTENURL:-http://*:8080}
+GERRIT_WEBURL=${1:-http://127.0.0.1/gerrit}
+LDAP_SERVER=${2:-127.0.0.1}
+LDAP_ACCOUNTBASE=${3:-ou=accounts,dc=demo,dc=com}
+HTTPD_LISTENURL=${4:-http://*:8080}
 GERRIT_NAME=${GERRIT_NAME:-gerrit}
 GERRIT_VOLUME=${GERRIT_VOLUME:-gerrit-volume}
 PG_GERRIT_NAME=${PG_GERRIT_NAME:-pg-gerrit}
@@ -34,6 +34,7 @@ echo "Create Gerrit volume."
 docker run \
 --name ${GERRIT_NAME} \
 --link ${PG_GERRIT_NAME}:db \
+--link ${LDAP_SERVER} \
 -p 29418:29418 \
 --volumes-from ${GERRIT_VOLUME} \
 -e WEBURL=${GERRIT_WEBURL} \
