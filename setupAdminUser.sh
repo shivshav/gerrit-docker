d504c6c42f61f7128c54e9e6e96de72de784e0ac
shiv <sprasad0603@gmail.com>
2016-03-11 22:29:42 -0800

    Remove jenkins user creation from gerrit-side script. ALso suppressed some output to clean up the docker logs. And did some much-needed code cleanup as well

--- setupAdminUser.sh
+++ setupAdminUser.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-set -x
+
+GERRIT_ADMIN_UID=$OPENLDAP_ENV_CI_ADMIN_UID
+GERRIT_ADMIN_PWD=$OPENLDAP_ENV_CI_ADMIN_PWD
 
 # Do first time login.
 RESPONSE=$(curl -H "X-Remote-User:${GERRIT_ADMIN_UID}" -u ${GERRIT_ADMIN_UID}:${GERRIT_ADMIN_PWD} -X GET http://localhost:8080/gerrit/login 2>/dev/null)
 echo "$?"
 #[ -z "${RESPONSE}" ] || { echo "${RESPONSE}" ; exit 1; }
+#!/bin/bash
+
+# Add ssh-key
+cat "${GERRIT_ADMIN_SSHKEY_PATH}" | curl -H X-Remote-User:${GERRIT_ADMIN_UID} --data @- --user "${GERRIT_ADMIN_UID}:${GERRIT_ADMIN_PWD}"  http://localhost:8080/gerrit/a/accounts/self/sshkeys
--- setupGerritJenkins.sh
+++ setupGerritJenkins.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
+GERRIT_ADMIN_EMAIL=${OPENLDAP_ENV_CI_ADMIN_EMAIL}
+CONFIG_WORKDIR=All-Projects
 
-docker exec ssh -i "${SSH_KEY_PATH}" -p 29418 ${GERRIT_ADMIN_UID}@${GERRIT_SSH_HOST} gerrit create-account --group "'Non-Interactive Users'" --full-name "'Jenkins Server'"
+# Clone from repo on disk
+mkdir $CONFIG_WORKDIR
+cd $CONFIG_WORKDIR
 
-#git config
-git config user.name  ${GERRIT_ADMIN_UID}
-git config user.email ${GERRIT_ADMIN_EMAIL}
-git remote add origin ssh://${GERRIT_ADMIN_UID}@${GERRIT_SSH_HOST}:29418/All-Projects 
+git init
 #checkout project.config
 git fetch -q origin refs/meta/config:refs/remotes/origin/meta/config
 git checkout meta/config
+
+# Setup committer username and email
+git config user.name  ${GERRIT_ADMIN_UID}
+git config user.email ${GERRIT_ADMIN_EMAIL}
 
 #add label.Verified
 git config -f project.config label.Verified.function MaxWithBlock
@@ -17,7 +22,7 @@ git config -f project.config --add label.Verified.value "-1 Fails"
 git config -f project.config --add label.Verified.value "0 No score"
 git config -f project.config --add label.Verified.value "+1 Verified"
 ##commit and push back
-git commit -a -m "Added label - Verified"
+git commit --quiet -a -m "Added label - Verified"
 
 #Change global access right
 ##Remove anonymous access right.
@@ -30,5 +35,8 @@ git config -f project.config --add access.refs/heads/*.label-Verified "-1..+1 gr
 ##add project owners' right to add verify flag
 git config -f project.config --add access.refs/heads/*.label-Verified "-1..+1 group Project Owners"
 ##commit and push back
-git commit -a -m "Change access right." -m "Add access right for Jenkins. Remove anonymous access right"
+git commit --quiet -a -m "Change access right." -m "Add access right for Jenkins. Remove anonymous access right"
 git push origin meta/config:meta/config
+
+cd ..
+rm -rf $CONFIG_WORKDIR
